292c81682432c88e409b0c424b9b513a4400dc93
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2022-12-20 06:49:08 +0000

    Layout

---
+++ src/home/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="heading">
+                Premier League Player Data
+            </div>
+            <div className="subheading">
+                This is the best place to stay upto date with your favourie players stats. <br />
+                Fill in the following filters to get started.
+            </div>
+        </div>
+    );
+}
+
+export default Home;
