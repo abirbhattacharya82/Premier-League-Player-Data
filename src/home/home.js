6b6fc245a513dfef1258eb83529c81b43fe43a90
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2022-12-21 13:23:21 +0000

    Image Job Done

--- src/home/home.js
+++ src/home/home.js
@@ -2,6 +2,27 @@ import React from "react";
 import './home.css';
 import Results from "../results/results";
 import { useState } from "react";
+import ars from "../results/1.png";
+import mci from "../results/2.png";
+import newc from "../results/3.png";
+import tot from "../results/4.png";
+import mnu from "../results/5.png";
+import liv from "../results/6.png";
+import bri from "../results/7.png";
+import che from "../results/8.png";
+import ful from "../results/9.png";
+import bre from "../results/10.png";
+import cry from "../results/11.png";
+import ast from "../results/12.png";
+import lei from "../results/13.png";
+import bou from "../results/14.png";
+import lee from "../results/15.png";
+import wha from "../results/16.png";
+import eve from "../results/17.png";
+import not from "../results/18.png";
+import sou from "../results/19.png";
+import wol from "../results/20.png";
+
 
 const Home = () => {
     const [flag, setFlag] = useState(false);
@@ -9,6 +30,7 @@ const Home = () => {
     const [position, setPosition] = useState("gk");
     const [appearances, setAppearances] = useState("opt1");
     const [goals, setGoals] = useState("opt1");
+    const [image_name, setImage] = useState("");
 
     return (
         <div className="home" id="home">
@@ -81,9 +103,71 @@ const Home = () => {
                         setPosition(document.getElementById("position").value);
                         setAppearances(document.getElementById("appearances").value);
                         setGoals(document.getElementById("goals").value);
+                        if (document.getElementById("team").value === "ars") {
+                            setImage(ars);
+                        }
+                        else if (document.getElementById("team").value === "mci") {
+                            setImage(mci);
+                        }
+                        else if (document.getElementById("team").value === "newc") {
+                            setImage(newc);
+                        }
+                        else if (document.getElementById("team").value === "tot") {
+                            setImage(tot);
+                        }
+                        else if (document.getElementById("team").value === "mnu") {
+                            setImage(mnu);
+                        }
+                        else if (document.getElementById("team").value === "liv") {
+                            setImage(liv);
+                        }
+                        else if (document.getElementById("team").value === "bri") {
+                            setImage(bri);
+                        }
+                        else if (document.getElementById("team").value === "che") {
+                            setImage(che);
+                        }
+                        else if (document.getElementById("team").value === "ful") {
+                            setImage(ful);
+                        }
+                        else if (document.getElementById("team").value === "bre") {
+                            setImage(bre);
+                        }
+                        else if (document.getElementById("team").value === "cry") {
+                            setImage(cry);
+                        }
+                        else if (document.getElementById("team").value === "ast") {
+                            setImage(ast);
+                        }
+                        else if (document.getElementById("team").value === "lei") {
+                            setImage(lei);
+                        }
+                        else if (document.getElementById("team").value === "bou") {
+                            setImage(bou);
+                        }
+                        else if (document.getElementById("team").value === "lee") {
+                            setImage(lee);
+                        }
+                        else if (document.getElementById("team").value === "wha") { 
+                            setImage(wha);
+                        }
+                        else if (document.getElementById("team").value === "eve") {
+                            setImage(eve);
+                        }
+                        else if (document.getElementById("team").value === "not") {
+                            setImage(not);
+                        }
+                        else if (document.getElementById("team").value === "sou") {
+                            setImage(sou);
+                        }
+                        else if (document.getElementById("team").value === "wol") {
+                            setImage(wol);
+                        }
+
                     }
                 }>check</button>
             </div>
+            <img src={image_name} alt="" />
             {flag ? <Results prop={[team,position,appearances,goals]} /> : null}
         </div>
     );
